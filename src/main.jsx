821e72683e37dceb234935ed9ebf1a7f97eecd8f
Sobdev <71671270+Sobdev@users.noreply.github.com>
2024-08-14 10:48:32 +0200

    Clean  migration to TailwindCSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './styles/tailwind.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
