a21d5e0eb8e0e366c685e468cedec45399eba937
Sobdev <71671270+Sobdev@users.noreply.github.com>
2024-08-17 11:54:05 +0200

    new design with TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,41 @@
-import { CardBasic } from './components/Cards/Cards.jsx';
-import { NavigationUser } from './components/Navigation/Navigation.jsx';
+import React, { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
 
-const App = () => {
+import 'aos/dist/aos.css';
+import './css/style.css';
+
+import AOS from 'aos';
+
+import Home from './pages/Home';
+
+function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 700,
+      easing: 'ease-out-cubic',
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
+
   return (
     <>
-      <NavigationUser />
-      <div className="relative overflow-hidden">
-        <img
-          src="https://www.gestioncolaborativa.es/img/trafico/matriculaciones-portada.jpg"
-          alt="Banner"
-          className="w-full h-48 object-cover bg-white"
-        />
-      </div>
-      <div className="container mx-auto px-4 py-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <CardBasic
-            title="¿Quiénes somos?"
-            text="Somos una Gestoría de Coslada"
-          />
-          <CardBasic
-            title="Nuestros servicios"
-            text="Gestionamos la matriculación de tu flota de furgonetas o tu turismo personal."
-            showButton={true}
-            buttonText="Ver más información"
-            variant="blue"
-          />
-          <CardBasic
-            title="Contáctanos"
-            showLinks={true}
-          />
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
-          <CardBasic
-            title="Horario"
-            text="L - V 9:00 - 18:00"
-          />
-          <CardBasic
-            title="Cómo llegar"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque."
-          />
-        </div>
-        <div className="mt-8">
-          <CardBasic
-            title="Card 1"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque."
-          />
-        </div>
-      </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Contact from '../Components/Contact';
+import Navigation from '../Components/Navigation';
+import Services from '../Components/Services';
+import ServiceBoxes from '../Components/ServicesBoxes';
+import HeroHome from '../components/HeroHome';
+
+
+function Home() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+
+      <Navigation />
+
+      <main className="flex-grow">
+
+        <HeroHome />
+        <Services />
+        <ServiceBoxes />
+        <Contact />
+
+      </main>
+
+    </div>
+  );
+}
+
+export default Home;
