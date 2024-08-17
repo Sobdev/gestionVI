34e1ddb858354f0cdeb2ade75eca05907f31cdda
Sobdev <71671270+Sobdev@users.noreply.github.com>
2024-08-17 13:37:33 +0200

    brand colors

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,6 @@ const Navigation = () => {
 
   const [top, setTop] = useState(true);
 
-  // detect whether user has scrolled the page down by 10px 
   useEffect(() => {
     const scrollHandler = () => {
       window.pageYOffset > 10 ? setTop(false) : setTop(true)
@@ -40,10 +39,13 @@ const Navigation = () => {
           <nav className="flex flex-grow">
             <ul className="flex flex-grow justify-end flex-wrap items-center">
               <li>
-                <Link to="https://wa.me/34653753291" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">WhatsApp</Link>
+                <Link to="https://wa.me/34653753291"
+                  className="btn-sm text-gray-200 bg-blue-500 hover:bg-teal-400 ml-3">
+                  WhatsApp</Link>
               </li>
               <li>
-                <Link to="https://maps.app.goo.gl/Fg6SFQh1brXDtzuD7" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                <Link to="https://maps.app.goo.gl/Fg6SFQh1brXDtzuD7"
+                  className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-500 ml-3">
                   <span>Cómo llegar</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
