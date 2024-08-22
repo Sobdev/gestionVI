37a9d7396f4b0190b05435753d09ab3cf3099f7b
Sobdev <71671270+Sobdev@users.noreply.github.com>
2024-08-22 15:34:00 +0200

    testing with the FeaturesExplained page

--- src/pages/FeaturesExplained.jsx
+++ src/pages/FeaturesExplained.jsx
@@ -2,7 +2,7 @@ import { Hero } from '../components/HeroHome';
 
 export const FeaturesExplained = () => {
     return (
-        <Section className="relative">
+        <section className="relative">
             <div className='flex flex-col min-h-screen overflow-hidden'>
                 <Hero
                     titleColored='Nuestros servicios'
@@ -11,8 +11,11 @@ export const FeaturesExplained = () => {
             </div>
             <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
                 <h2 className="h2 mb-4">Nuestro proceso es fácil y eficiente</h2>
+                <p className="h4 text-xl text-gray-600">Deja tus trámites en nuestras manos y disfruta de una gestión sin complicaciones</p>
                 <p className="text-xl text-gray-600">Deja tus trámites en nuestras manos y disfruta de una gestión sin complicaciones</p>
+                <p className="text-xl text-gray-600">Deja tus trámites en nuestras manos y disfruta de una gestión sin complicaciones</p>
+
             </div>
-        </Section>
+        </section>
     )
 }
